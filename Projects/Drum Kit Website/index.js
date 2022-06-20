598365ed18e1df5ad54f0f19ff2c7e17e86ada59
Fernando <fieldy.senpai@gmail.com>
2022-06-20 22:01:36 +0100

    Added event listener to drum class

--- Projects/Drum Kit Website/index.js
+++ Projects/Drum Kit Website/index.js
@@ -1,3 +1,9 @@
-function handleClick() {
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+
+  function handleClick() {
     alert("I got clicked");
+  }
 }
